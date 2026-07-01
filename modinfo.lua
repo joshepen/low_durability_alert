504fcbdeee92d0926432b63ca347975cdf534ccd
Josh <pennerthing2@gmail.com>
2026-07-01 14:50:01 -0500

    Initial Commit

---
+++ modinfo.lua
@@ -0,0 +1,8 @@
+name = "Low Durability Alert"
+description = "Alerts you when a tool or worn item has low durability"
+author = "joshepen"
+version = "1.0"
+dst_compatible = true
+api_version = 10
+client_only_mod = true
+all_clients_require_mod = false
