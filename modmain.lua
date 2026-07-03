ad848fd83af3eb2e06a1a4004915ca5b531e2f04
Josh <pennerthing2@gmail.com>
2026-07-03 00:03:48 -0500

    Add functions for watching and unwatching and checking, etc

--- modmain.lua
+++ modmain.lua
@@ -1,14 +1,64 @@
+print("LDA: LOADED")
 AddPlayerPostInit(function(inst)
     inst:ListenForEvent("playeractivated", function()
-        if inst == GLOBAL.ThePlayer then
-            print("This is the local player!")
-            if inst.replica.inventory then
-                print("Has inventory component")
-                local item = inst.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HANDS)
-                print("Got item:", item)
+        if inst ~= GLOBAL.ThePlayer then return end -- only run for the local client's own player
+
+        local warned = {}
+
+        local function GetDurabilityPercent(item)
+            local ii = item.replica.inventoryitem
+            local classified = ii and ii.classified
+            if classified and classified.percentused:value() ~= 255 then
+                return classified.percentused:value() / 100
+            end
+            return nil
+        end
+
+        local function CheckItem(item)
+            if item == nil then return end
+            local percent = GetDurabilityPercent(item)
+            print("LDA: CHECKING ITEM", item, "percent:", percent, "finiteuses:", item.replica.finiteuses, "armor:", item.replica.armor)
+
+            if percent == nil then return end
+
+            if percent < 0.80 then
+                if not warned[item] then
+                    warned[item] = true
+                    print("LDA: PLAYING SOUND")
+                    GLOBAL.ThePlayer.SoundEmitter:PlaySound("dontstarve/common/horn_beefalo")
+                end
             else
-                print("No inventory component yet")
+                warned[item] = nil
             end
         end
+
+        local function WatchItem(item)
+            print("LDA: WATCHING ITEM", item)
+            if item == nil then return end
+            inst:ListenForEvent("percentusedchange", function() CheckItem(item) end, item)
+            CheckItem(item)
+        end
+
+        local function UnwatchItem(item)
+            print("LDA: UNWATCH ITEM", item)
+            if item == nil then return end
+            inst:RemoveEventCallback("percentusedchange", nil, item)
+            warned[item] = nil
+        end
+
+        print("LDA: PLAYER ACTIVATED")
+        inst:ListenForEvent("equip", function(_, data) WatchItem(data.item) end)
+        inst:ListenForEvent("unequip", function(_, data) UnwatchItem(data.item) end)
+
+        -- Add for currently equipped items
+        local equipped_weapon = inst.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HANDS)
+        WatchItem(equipped_weapon)
+
+        local equipped_armor = inst.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.BODY)
+        WatchItem(equipped_armor)
+
+        local equipped_head = inst.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HEAD)
+        WatchItem(equipped_head)
+
     end)
 end)
