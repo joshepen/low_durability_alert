2c3d35062258d0d4d9ab8185f7473e01c02dc7f4
Josh <pennerthing2@gmail.com>
2026-07-02 20:05:22 -0500

    Get logs working on world start

--- modmain.lua
+++ modmain.lua
@@ -1,1 +1,14 @@
-print("Hello World - My First DST Mod!")
+AddPlayerPostInit(function(inst)
+    inst:ListenForEvent("playeractivated", function()
+        if inst == GLOBAL.ThePlayer then
+            print("This is the local player!")
+            if inst.replica.inventory then
+                print("Has inventory component")
+                local item = inst.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HANDS)
+                print("Got item:", item)
+            else
+                print("No inventory component yet")
+            end
+        end
+    end)
+end)
